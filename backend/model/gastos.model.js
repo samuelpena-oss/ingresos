28fea2624e91d6051491e95242b45d94b3e782f0
samuel <samuel.pena@pi.edu.co>
2026-08-27 18:24:57 -0500

    cre

--- backend/model/gastos.model.js
+++ backend/model/gastos.model.js
@@ -1,6 +1,7 @@
 const db = require("../confing/db")
 class GastosModel{
 static async consultarGastos(){
-       
+       const resultados = await db.query("SELECT * FROM gastos");
+       return resultados
 }
 }
