bab13ad2f49fa5b0478508f514d16126a264e25b
samuel <samuel.pena@pi.edu.co>
2026-09-03 19:06:54 -0500

    categorias

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -2,4 +2,5 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/gastos.controller")
 router.get("/categorias", controller.consultarGastos)
+router.get("/transacciones", controller.consultarGastos)
 module.exports = router
